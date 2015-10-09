672fc4cc85450ebc65965fdd2dd18482e930678d
Iurii Kyrylenko <iurii_kyrylenko@epam.com>
2015-10-09 16:36:19 +0300

    Updated gj-pages.

--- fdraw/iterateMbr.js
+++ fdraw/iterateMbr.js
@@ -3,21 +3,19 @@
 
    var fdrawModule = angular.module('fdrawModule');
 
-   fdrawModule.factory('iterateMbr', function(){
-      return function(c, maxIter) {
-         var z = {x:0, y:0};
-         for(var i = 0 ;i < maxIter; i++) {
-            var zz = {
-               x: z.x*z.x - z.y*z.y + c.x,
-               y: 2*z.x*z.y + c.y
-            };
-            if(zz.x*zz.x + zz.y*zz.y > 4) {
-               return i;
-            }
-            z = zz;
+   fdrawModule.value('iterateMbr', function(c, maxIter) {
+      var z = {x:0, y:0};
+      for(var i = 0; i < maxIter; i++) {
+         var zz = {
+            x: z.x*z.x - z.y*z.y + c.x,
+            y: 2*z.x*z.y + c.y
+         };
+         if(zz.x*zz.x + zz.y*zz.y > 4) {
+            return i;
          }
-         return maxIter;
-      };
+         z = zz;
+      }
+      return maxIter;
    });
 
 })();
